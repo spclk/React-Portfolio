38f90ce513902b5079c49cb7c81b0376c73d764d
Konstantin <konstantin1020@gmail.com>
2021-05-13 14:40:35 -0700

    installed materialize

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import 'materialize-css/dist/css/materialize.min.css'
 
 function App() {
   return (
