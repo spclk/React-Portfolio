13235e06d167774f83de212a371ffbd6b8f45376
Konstantin <konstantin1020@gmail.com>
2021-05-13 14:21:42 -0700

    added components

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./style.css";
+
+
+function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <a className="navbar-brand" href="/">
+        
+      </a>
+    </nav>
+  );
+}
+
+export default Navbar;
