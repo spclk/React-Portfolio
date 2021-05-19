157d211adf807655f5fc0c653ef760c75f65c807
Konstantin <konstantin1020@gmail.com>
2021-05-19 16:25:28 -0700

    added portfolio content

--- src/components/pages/portfolio.js
+++ src/components/pages/portfolio.js
@@ -2,45 +2,64 @@ import React from "react";
 
 function Portfolio() {
   return (
-    <main>
-      <div className="section white">
-        <div className="row container">
-          <h6 className="grey-text text-darken-3 lighten-3">
-            I'm Konstantin Alekseev, a Full-Stack Web Developer in Folsom, CA. I
-            love coding because it allows me to solve complex challenges and
-            rewards me with a thrill of creating things into existence. I’m
-            excited about helping individuals and companies to display the best
-            presence in the digital world of today. I'm super passioante about
-            making contributions to developing tools that make this world a
-            better place to live in. As a curious person, I love to learn new
-            things. Feel free to reach out to me.
-          </h6>
-        </div>
+    <main class="grey lighten-1">
+      <div class="section white">
       </div>
-      <div >
-        <div >
-          <img src="../images/Portfolio.png" alt="digital" />
+        <div class="row container">
+          <h3 class="header">Portfolio</h3>
+          <p class="grey-text text-darken-3 lighten-3">This is only a sample of the projects I've completed at UC Davis Coding Bootcamp. See more on my GitHub page.</p>
         </div>
-      </div>
-      <div className="section white">
-        <div className="row container">
-          <h3 className="header">Portfolio</h3>
-          <h6 className="grey-text text-darken-3 lighten-3">
-            Check out some of the the things I've worked on at the UC Davis
-            Coding Bootcamp.{" "}
-            <a href="portfolio.html" className="blue-grey-text text-darken-4">
-              CLICK HERE
-            </a>
-          </h6>
+        <div class="container">
+                <div class="row">
+                    <div class="col s4">
+                        <div class="card">
+                            <div class="card-image waves-effect waves-block waves-light">
+                              <img class="activator" src="images/cryptelligent.png"/>
+                            </div>
+                            <div class="card-content">
+                              <span class="card-title activator grey-text text-darken-4">Cryptelligent<i class="material-icons right">more_vert</i></span>
+                              <p><a href="https://spclk.github.io/Cryptelligent/">Link to Website</a></p>
+                            </div>
+                            <div class="card-reveal">
+                              <span class="card-title grey-text text-darken-4">Cryptelligent<i class="material-icons right">close</i></span>
+                              <p>This is an online application for new investors who wish to learn more about the world of cryptocurrency.</p> 
+                              <p>CoinGecko and Bloomberg APIs are utilized to provide the cryptocurrency information.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col s4">
+                        <div class="card">
+                            <div class="card-image waves-effect waves-block waves-light">
+                              <img class="activator" src="images/Day-Planner.png"/>
+                            </div>
+                            <div class="card-content">
+                              <span class="card-title activator grey-text text-darken-4">Day Planner<i class="material-icons right">more_vert</i></span>
+                              <p><a href="https://spclk.github.io/day-planner/">Link to Website</a></p>
+                            </div>
+                            <div class="card-reveal">
+                              <span class="card-title grey-text text-darken-4">Day Planner<i class="material-icons right">close</i></span>
+                              <p>This is a simple calendar application that allows a user to save events for each hour of the day at work.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col s4">
+                        <div class="card">
+                            <div class="card-image waves-effect waves-block waves-light">
+                              <img class="activator" src="images/Projectimator.jpg"/>
+                            </div>
+                            <div class="card-content">
+                              <span class="card-title activator grey-text text-darken-4">Projectimator<i class="material-icons right">more_vert</i></span>
+                              <p><a href="https://arcane-dusk-72745.herokuapp.com/">Link to Website</a></p>
+                            </div>
+                            <div class="card-reveal">
+                              <span class="card-title grey-text text-darken-4">Projectimator<i class="material-icons right">close</i></span>
+                              <p>Provids an online service for Homeowners to locate contractors that are able to work within a specified budget.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>    
         </div>
-      </div>
-      <br />
-      <br />
-      <div className="parallax-container">
-        <div className="parallax">
-          <img src="images/keyboard binery numbers.jpg" alt="keyboard" />
-        </div>
-      </div>
+      
     </main>
   );
 }
