7a2513fedaa9584d96abe730a2a6879b1157b48d
Konstantin <konstantin1020@gmail.com>
2021-05-19 16:30:54 -0700

    updated classes

--- src/components/pages/portfolio.js
+++ src/components/pages/portfolio.js
@@ -2,57 +2,57 @@ import React from "react";
 
 function Portfolio() {
   return (
-    <main class="grey lighten-1">
-      <div class="section white">
+    <main className="grey lighten-1">
+      <div className="section white">
       </div>
-        <div class="row container">
-          <h3 class="header">Portfolio</h3>
-          <p class="grey-text text-darken-3 lighten-3">This is only a sample of the projects I've completed at UC Davis Coding Bootcamp. See more on my GitHub page.</p>
+        <div className="row container">
+          <h3 className="header">Portfolio</h3>
+          <p className="grey-text text-darken-3 lighten-3">This is only a sample of the projects I've completed at UC Davis Coding Bootcamp. See more on my GitHub page.</p>
         </div>
-        <div class="container">
-                <div class="row">
-                    <div class="col s4">
-                        <div class="card">
-                            <div class="card-image waves-effect waves-block waves-light">
-                              <img class="activator" src="images/cryptelligent.png" alt="screenshot of website"/>
+        <div className="container">
+                <div className="row">
+                    <div className="col s4">
+                        <div className="card">
+                            <div className="card-image waves-effect waves-block waves-light">
+                              <img className="activator" src="images/cryptelligent.png" alt="screenshot of website"/>
                             </div>
-                            <div class="card-content">
-                              <span class="card-title activator grey-text text-darken-4">Cryptelligent<i class="material-icons right">more_vert</i></span>
+                            <div className="card-content">
+                              <span className="card-title activator grey-text text-darken-4">Cryptelligent<i className="material-icons right">more_vert</i></span>
                               <p><a href="https://spclk.github.io/Cryptelligent/">Link to Website</a></p>
                             </div>
-                            <div class="card-reveal">
-                              <span class="card-title grey-text text-darken-4">Cryptelligent<i class="material-icons right">close</i></span>
+                            <div className="card-reveal">
+                              <span className="card-title grey-text text-darken-4">Cryptelligent<i className="material-icons right">close</i></span>
                               <p>This is an online application for new investors who wish to learn more about the world of cryptocurrency.</p> 
                               <p>CoinGecko and Bloomberg APIs are utilized to provide the cryptocurrency information.</p>
                             </div>
                         </div>
                     </div>
-                    <div class="col s4">
-                        <div class="card">
-                            <div class="card-image waves-effect waves-block waves-light">
-                              <img class="activator" src="images/Day-Planner.png" alt="screenshot of website"/>
+                    <div className="col s4">
+                        <div className="card">
+                            <div className="card-image waves-effect waves-block waves-light">
+                              <img className="activator" src="images/Day-Planner.png" alt="screenshot of website"/>
                             </div>
-                            <div class="card-content">
-                              <span class="card-title activator grey-text text-darken-4">Day Planner<i class="material-icons right">more_vert</i></span>
+                            <div className="card-content">
+                              <span className="card-title activator grey-text text-darken-4">Day Planner<i className="material-icons right">more_vert</i></span>
                               <p><a href="https://spclk.github.io/day-planner/">Link to Website</a></p>
                             </div>
-                            <div class="card-reveal">
-                              <span class="card-title grey-text text-darken-4">Day Planner<i class="material-icons right">close</i></span>
+                            <div className="card-reveal">
+                              <span className="card-title grey-text text-darken-4">Day Planner<i className="material-icons right">close</i></span>
                               <p>This is a simple calendar application that allows a user to save events for each hour of the day at work.</p>
                             </div>
                         </div>
                     </div>
-                    <div class="col s4">
-                        <div class="card">
-                            <div class="card-image waves-effect waves-block waves-light">
-                              <img class="activator" src="images/Projectimator.jpg" alt="screenshot of website"/>
+                    <div className="col s4">
+                        <div className="card">
+                            <div className="card-image waves-effect waves-block waves-light">
+                              <img className="activator" src="images/Projectimator.jpg" alt="screenshot of website"/>
                             </div>
-                            <div class="card-content">
-                              <span class="card-title activator grey-text text-darken-4">Projectimator<i class="material-icons right">more_vert</i></span>
+                            <div className="card-content">
+                              <span className="card-title activator grey-text text-darken-4">Projectimator<i className="material-icons right">more_vert</i></span>
                               <p><a href="https://arcane-dusk-72745.herokuapp.com/">Link to Website</a></p>
                             </div>
-                            <div class="card-reveal">
-                              <span class="card-title grey-text text-darken-4">Projectimator<i class="material-icons right">close</i></span>
+                            <div className="card-reveal">
+                              <span className="card-title grey-text text-darken-4">Projectimator<i className="material-icons right">close</i></span>
                               <p>Provids an online service for Homeowners to locate contractors that are able to work within a specified budget.</p>
                             </div>
                         </div>
